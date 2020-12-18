eba1d6629d5dae0fcdc744ffc576ae87c84ca38e
日置 智紀 <hioki@sec.co.jp>
2020-12-18 17:15:43 +0900

    second commit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        System.out.println("ハローワールドから変更");
+        System.out.println("ハローワールドから変更２");
         //return 0;
     }
 
