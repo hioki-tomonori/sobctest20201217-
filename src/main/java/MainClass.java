21be97526c865a2dc4b937255ffdf4dfa28d2821
日置 智紀 <hioki@sec.co.jp>
2020-12-18 17:12:50 +0900

    first commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+public class MainClass {
+    /**
+     * Inserts the objects in the specified collection at the specified location
+     * in this List. The objects are added in the order they are returned from
+     * the collection's iterator.
+     *
+     * @param args コマンドライン引数
+     * @throws IndexOutOfBoundsException when {@code location < 0 || location > size()}
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("ハローワールドから変更");
+        //return 0;
+    }
+
+}
